c1959b207e0aff0dbdd723dbd825a1a98734b32e
Yonah Goldberg <yonah.goldberg@icloud.com>
2024-04-14 22:46:24 -0400

    man

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -55,4 +55,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/cilk.dir/DependInfo.cmake"
+  "CMakeFiles/test.dir/DependInfo.cmake"
   )

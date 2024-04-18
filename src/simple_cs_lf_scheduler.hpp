c758419cc683531df4d925e76d117ba246d5d408
Jack Ellinger <jackellinger@gmail.com>
2024-04-18 11:29:39 -0400

    fixed?

--- src/simple_cs_lf_scheduler.hpp
+++ src/simple_cs_lf_scheduler.hpp
@@ -5,27 +5,26 @@
 #include <future>
 #include <iostream>
 #include <stdatomic.h>
-#include "lock-free-queue/JobQueue.h"
+#include "lock-free-queue/JobQueue.hpp"
+#include "lock-free-queue/Task.hpp"
 #include <queue>
 #include <thread>
 #include <vector>
-
 #include "scheduler.hpp"
 
 template <typename T> class SimpleCSLFScheduler : public Scheduler<T> {
 private:
   // A task that a thread can run.
-  struct Task {
-    std::packaged_task<T()> func;
-  };
+  // struct Task {
+  //   std::packaged_task<T()> func;
+  // };
   // All the threads in the thread pool
   std::vector<std::thread> threads;
   // Map from std::thread::id to an integer thread id that is easier to work
   // with.
   std::unordered_map<std::thread::id, int> threadIds;
   // Each thread has an associated queue of tasks for it to run.
-  std::vector<JobQueue *> taskQueues; // task queue for each worker
-  std::vector<Worker *> workers; // worker 
+  std::vector<JobQueue<T> *> taskQueues; // task queue for each worker
   // Each task queue has an associated mutex for accessing.
   // std::vector<int8_t> locks;
   int n; // The number of threads in thread pool
@@ -43,43 +42,52 @@ public:
     // Add the rest of the workers
     for (size_t i = 0; i < n; i++)
     {
-      JobQueue *queue = new JobQueue(100);
+      JobQueue<T> *queue = new JobQueue<T>(100);
       taskQueues.push_back(queue);
-      threads.emplace_back(&SimpleCSLFcheduler::workerThread, this, i);
+      threads.emplace_back(&SimpleCSLFScheduler::workerThread, this, i);
       threadIds[threads[i].get_id()] = i;
     }
+    // printf("GOT PAST INIT\n");
   }
 
   // Spawn new function to potentially be run in parallel.
   // This function gets stored on this thread's task queue and can be stolen
   // later by this thread, or another thread if another thread runs out of work.
   std::future<T> spawn(std::function<T()> func) {
+    // printf("STARTED SPAWN\n");
     std::packaged_task<T()> task(func);
     int tid = getTid();
     auto fut = task.get_future();
-
+    // printf("GOT FUTURE\n");
     {
       // Lock current thread's task queue before accessing
-      taskQueues[tid]->Push(Task{std::move(task)})
+      // printf("START PUSH IN SPAWN\n");
+      taskQueues[tid]->Push(new Task<T>{std::move(task)});
+      // printf("PUSHED IN SPAWN\n");
     }
     return fut;
   }
 
-  JobQueue* GetRandomJobQueue()
+  JobQueue<T>* GetRandomJobQueue()
   {
-      static std::random_device rd;
-      static std::mt19937 gen(rd());
-      static std::uniform_int_distribution<> distribution(0, n);
-
+    // printf("GET RANDOM QUEUE\n");
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> distribution(1, n);
     size_t index = static_cast<size_t> ( std::round( distribution( gen ) ) );
-    return taskQueues[index];
+    // printf("%d\n", index);
+    JobQueue<T>* queue = taskQueues[index - 1];
+    // printf("GET RANDOM QUEUE FINISHED\n");
+    return queue;
   }
 
-  Task* getTask(int curTid) {
-    JobQueue* queue = taskQueues[curTid];
-    Task* task = queue->Pop();
+  Task<T>* getTask(int curTid) {
+    // printf("GET TASK \n");
+    JobQueue<T>* queue = taskQueues[curTid];
+    Task<T>* task = queue->Pop();
+    // printf("GET TASK POPPED\n");
     if (task == nullptr){
-      JobQueue *randomQueue = GetRandomJobQueue();
+      JobQueue<T> *randomQueue = GetRandomJobQueue();
       if (randomQueue == nullptr)
       {
         std::this_thread::yield();
@@ -103,16 +111,25 @@ public:
 
   // Attempt to steal work while waiting on fut to finish
   T steal(std::future<T> fut) {
+    // printf("STEAL START\n");
     int curTid = getTid();
 
     // While future is not valid, attempt to steal work
     while (fut.wait_for(std::chrono::milliseconds(0)) !=
            std::future_status::ready) {
-      Task* task = getTask(curTid);
+      Task<T>* task = getTask(curTid);
       if (task != nullptr) {
-        task->func();
+        // task->func();
         // might need to do parent dependencies and stuff
+        try {
+          task->func();  // Execute the task
+        } catch (const std::exception& e) {
+          printf("ERROR ALERT");
+          // Handle the exception or continue
+        }
       }
+
+      
     }
     // Return result of future if there is one
     if constexpr (std::is_void<T>::value) {
@@ -138,15 +155,21 @@ public:
 
 private:
   // Get the callling threads integer thread ID
-  Worker getWorker() { return threadIdToWorker[std::this_thread::get_id()]; }
+  int getTid() { return threadIds[std::this_thread::get_id()]; }
 
   void workerThread(int tid) {
     int curTid = getTid();
 
     while (!finish) {
-      Task* task = getTask(curTid);
+      Task<T>* task = getTask(curTid);
       if (task != nullptr) {
         task->func();
+        // try {
+        //   task->func();  // Execute the task
+        // } catch (const std::exception& e) {
+        //   // printf("%d\n", curTid);
+        //   // Handle the exception or continue
+        // }
         // might need to do parent dependencies and stuff
       }
     }
